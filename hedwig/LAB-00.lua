7cb3f7821022977813d96bfc5e29b9c3173cdd94
Yorokobi <kshitijyadav651@gmail.com>
2026-08-10 10:30:52 +0530

    hedwig: LAB00

--- hedwig/LAB-00.lua
+++ hedwig/LAB-00.lua
@@ -9,12 +9,22 @@ function mathematica(x, y, operator)
 		return x*y
 
 	elseif operator == "/" then
-		return x/y
+		if y ~= 0 then
+			return x/y
+		else
+			return
+		end
 	end
 
+	return nil
 end
 
-print(10,5,"+", mathematica(10, 5, "+"))
-print(10,5,"-", mathematica(10, 5, "-"))
-print(10,5,"*", mathematica(10, 5, "*"))
-print(10,5,"/", mathematica(10, 5, "/"))
+repeat
+	io.write("X: "); local x = tonumber(io.read())
+	io.write("Y: "); local y = tonumber(io.read())
+	io.write("Operator: "); local operator = io.read()
+	print(string.format("%g\t%g\t%g\n", x, y, mathematica(x, y, operator)))
+
+	io.write("Continue? (0/1)\n::> ")
+	checkForLoop = tonumber(io.read())
+until checkForLoop == 0
