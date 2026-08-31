1e69d0549ad23f584d9393b5d0f0727f967f4a77
Yorokobi <kshitijyadav651@gmail.com>
2026-08-31 10:32:16 +0000

    hedwig/ludics

--- hedwig/ludics/0x00/main.lua
+++ hedwig/ludics/0x00/main.lua
@@ -1,28 +1,51 @@
 function love.load()
 	love.window.setTitle("0x00 -- Initializing Love2D")
+	love.graphics.setBackgroundColor(0.12, 0.15, 0.18)
 
 	object = {
-		mode = "fill",
-		x = 100,
-		y = 0,
-		width = 50,
-		height = 50,
+		sprite = love.graphics.newImage("sprite.jpg"),
+		x = 0,
+		y = 0
 	}
+
+	object.width = object.sprite:getWidth()
+  object.height = object.sprite:getHeight()
 end
 
-function love.update()
-	--TODO: (1) Bounds for traversing over the canvas.
-	--(2) Better colorscheme
-	--(3) Sprite and Mouse Click
-	if love.keyboard.isDown("right") and object.x < 500 then
-		object.x = object.x + 10
+function love.update(dt)
+	if love.keyboard.isDown("right") then
+		object.x = object.x + 500 * dt
+	elseif love.keyboard.isDown("left") then
+		object.x = object.x - 500 * dt
 	end
 
-	if love.keyboard.isDown("left") and object.x > 100 then
-		object.x = object.x - 10
+	if love.keyboard.isDown("up") then
+		object.y = object.y - 500 * dt
+	elseif love.keyboard.isDown("down") then
+		object.y = object.y + 500 * dt
 	end
+
+	object.x = math.max(0, math.min(object.x, love.graphics.getWidth() - object.width))
+	object.y = math.max(0, math.min(object.y, love.graphics.getHeight() - object.height))
 end
 
 function love.draw()
-	love.graphics.rectangle(object.mode, object.x, object.y, object.width, object.height)
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.draw(object.sprite, object.x, object.y)
+end
+
+--[[
+--User-Defined Functions
+--(1) Closing Windows.
+--]]
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == "escape" or key == "q" then
+		love.event.quit()
+	end
+
+	if key == "f" then
+		fullscreen = not fullscreen
+		love.window.setFullscreen(fullscreen, "exclusive")
+	end
 end
