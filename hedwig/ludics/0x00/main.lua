1e9bdb7c0f955df79112ef02ed65663efd79eca1
Yorokobi <kshitijyadav651@gmail.com>
2026-09-09 16:59:58 +0000

    0x00/ANIMATION

--- hedwig/ludics/0x00/main.lua
+++ hedwig/ludics/0x00/main.lua
@@ -1,15 +1,53 @@
 function love.load()
+	ANIM = require 'libraries/anim8'
+
 	love.window.setTitle("0x00 -- Initializing Love2D")
 	love.graphics.setBackgroundColor(0.12, 0.15, 0.18)
-	--TODO:Adding Spritesheet!
+	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	object = {
+		x = 100,
+		y = 100,
+		velocity = 10,
+		atlas = love.graphics.newImage('object.png'),
+	}
+	
+	object.grid = ANIM.newGrid(12, 18, object.atlas:getWidth(), object.atlas:getHeight())
+	object.animations = {
+		idle = ANIM.newAnimation(object.grid('1-4', '1-4'), 0.2),
+		down = ANIM.newAnimation(object.grid('1-4', 1), 0.2),
+		left = ANIM.newAnimation(object.grid('1-4', 2), 0.2),
+		right = ANIM.newAnimation(object.grid('1-4', 3), 0.2),
+		up = ANIM.newAnimation(object.grid('1-4', 4), 0.2),
+	}
+	object.state = object.animations.idle
 end
 
 function love.update(dt)
-	--TODO:Motion for Spritesheet!
+	if love.keyboard.isDown("up") then
+		object.y = object.y - object.velocity
+		object.state = object.animations.up
+	elseif love.keyboard.isDown("down") then
+		object.y = object.y + object.velocity
+		object.state = object.animations.down
+	end
+
+	if love.keyboard.isDown("right") then
+		object.x = object.x + object.velocity
+		object.state = object.animations.right
+	elseif love.keyboard.isDown("left") then
+		object.x = object.x - object.velocity
+		object.state = object.animations.left
+	end
+
+	object.state:update(dt)
+
+	object.x = math.max(0, math.min(object.x, love.graphics.getWidth() - object.atlas:getWidth()))
+	object.y = math.max(0, math.min(object.y, love.graphics.getHeight() - object.atlas:getHeight()))
 end
 
 function love.draw()
-	--TODO:Drawing Spritesheet!
+	object.state:draw(object.atlas, object.x, object.y, nil, 7)
 end
 
 --[[Handling Keypresses]]
